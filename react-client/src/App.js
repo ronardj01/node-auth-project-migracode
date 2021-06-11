e57926bdb0b45d931460c7d70f4117a08ca80107
ronardj01 <ronardj01@gmail.com>
2021-06-11 14:12:04 +0200

    Added React Router

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -3,14 +3,32 @@ import LoginPage from './components/login-register/LoginPage';
 import 'bootstrap/dist/css/bootstrap.css'
 import Register from './components/login-register/Register';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 function App() {
+
   return (
     <div className="App">
-      <div className='login-singin'>
-      <LoginPage/>
-      </div>
-     
-    </div>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <div className='login-singin'>
+              <LoginPage />
+            </div>
+          </Route>
+
+          <Route path="/register">
+            <div className='register'>
+              <Register />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </div >
   );
 }
 
